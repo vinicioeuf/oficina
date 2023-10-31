60cad3dc041fc664292324e0639067352ff1de3d
Vinicio Eufrazio <vinicio.eufrazio@aluno.ifsertao-pe.edu.br>
2023-10-31 15:07:09 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,25 @@
+<?php
+
+class Conexao{
+    public static $instance;
+    
+    private function __construct() {
+        
+    }
+    
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new PDO('mysql:host=bntocbesetwryajamvft-mysql.services.clever-cloud.com;
+           dbname=bntocbesetwryajamvft',
+                'uki57199bcxapacs', 'OAx1sVs7K76wKMnSkqMy');
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS,
+                PDO::NULL_EMPTY_STRING);
+        }
+        
+        return self::$instance;
+    }
+    
+}
+?>
