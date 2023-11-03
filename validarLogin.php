86af4edbd05b5d0b9141911098fc0c14253a0331
VictorC78 <victorcarlos0000000000@gmail.com>
2023-11-03 20:45:08 -0300

    name

--- validarLogin.php
+++ validarLogin.php
@@ -8,21 +8,17 @@ $email = addslashes($_POST['email']);
 $senha = addslashes($_POST['senha']);
 
 $usu = new registroDao();    
+$usuLogado = $usu->login($email, $senha);   
 
-$senhacorreta= $usu->senha($email);
-
-if(password_verify($senha,$senhacorreta['senha'])){ ///erro se nao colocar email valido
-    $usuLogado = $usu->login($email, $senhacorreta['senha']);   
-    if ($usuLogado) { 
+if ($usuLogado) { 
         $_SESSION ['nome'] = $usuLogado['nome'];
         $_SESSION ['sobrenome'] = $usuLogado['sobrenome'];
         $_SESSION['email'] = $email;
         $_SESSION['idUser'] = $usuLogado['id'];
         
-        header('Location: login.php');
-         
-        }
-}else{
+        header('Location: oficina.php');
+  }
+else{
     unset($_SESSION['email']);
     unset($_SESSION['senha']);
    echo "<script>window.alert('Não há usuarios cadastrados com as credenciais informadas!')
