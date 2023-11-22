8f7a44ec2a5ad49d5f61485f5c8d89e905afb8e1
VictorC78 <victorcarlos0000000000@gmail.com>
2023-11-22 09:27:47 -0300

    .

--- conexao.php
+++ conexao.php
@@ -8,8 +8,8 @@ class Conexao
     {
         if(!isset(self::$instance))
         {
-            self::$instance = new PDO('mysql:host=bntocbesetwryajamvft-mysql.services.clever-cloud.com;
-            dbname=bntocbesetwryajamvft', 'uki57199bcxapacs', 'OAx1sVs7K76wKMnSkqMy');
+            self::$instance = new PDO('mysql:host=bbjdiciefhvh92vwvb9n-mysql.services.clever-cloud.com;
+            dbname=bbjdiciefhvh92vwvb9n', 'ufxdjocb4xr0aejr', 'PC6bn4cSDealeFoKw0zU');
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
